06a70db068c6aec3fc20742888326d00e448f0d9
palromproductssrl-sys <palromproductssrl@gmail.com>
2026-07-01 10:44:43 +0200

    Verhoog versienummer naar 5.9 (next.config.mjs)

--- next.config.mjs
+++ next.config.mjs
@@ -8,11 +8,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.supabase.co',
-        pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
         pathname: '/**',
       },
